b0e7db48c048ac7d14ce9f71924aa8d2f1f9559e
FedericaC03 <fdrk03@hotmail.it>
2020-10-02 16:19:15 +0200

    fine esercizio

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
 <?php
 $paragraph = 'lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-$badwords = $_GET['lorem'];
-$newParagraph = str_replace('lorem', "***", $paragraph);
-// $newParagraph = str_replace($badwords, "***", $paragraph);
+$badword = $_GET["word"];
+$newParagraph = str_replace($badword, "***", $paragraph);
 
 ?>
 
